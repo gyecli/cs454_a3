47f921624fae13af5565baa9e9d4ad4723443171
Tim Ye <gye@uwaterloo.ca>
2013-06-27 12:28:06 -0400

    rcp functions come here(?)

---
+++ rpc.cpp
@@ -0,0 +1,8 @@
+// Please put the rcp functions here
+
+int rpcInit();
+int rpcCall(char* name, int* argTypes, void** args);
+int rpcCacheCall(char* name, int* argTypes, void** args);
+int rpcRegister(char* name, int* argTypes, skeleton f);
+int rpcExecute();
+int rpcTerminate();
