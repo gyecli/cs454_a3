ffba3a44ed23aeb81cef394ee740191e8a844200
gyecli <gye@uwaterloo.ca>
2013-07-09 12:27:05 -0400

    Nothing

--- my_rpc.h
+++ my_rpc.h
@@ -1,14 +1,18 @@
 
-
+//DODO how should we define all these ints 
 #define REGISTER 1
 #define LOC_REQUEST 2
 #define LOC_SUCCESS 3
 #define EXECUTE 4
 #define EXECUTE_SUCCESS 5
 #define FAILURE 0
+<<<<<<< HEAD
 
 #define RPCCALL_SUCCESS 0
 #define RPCCALL_FAILURE -1
+=======
+#define LOC_FAILURE 6
+>>>>>>> b0e3a049fcba7b74841f32b05913cfe0cba47710
 
 #define IO_mask (3 << 30)
 #define Input_mask (1 << 31)
@@ -17,3 +21,6 @@
 #define Length_mask 65535			// OR 65535 (NOT SURE, becasue it's last 2 bytes)
 
 //define reasonCode here	
+
+//added by Yiyao 
+#define REGISTER_SUCCESS 0; 
